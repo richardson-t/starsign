bc3ff156c2f157120ce5eb9199fdf497819b4441
Theo Richardson <richardson.t@ufl.edu>
2023-07-10 16:16:28 -0500

    add coordinate finder

---
+++ make_coord.py
@@ -0,0 +1,12 @@
+from astropy import units as u
+from astropy.coordinates import SkyCoord,AltAz,EarthLocation
+from astropy.time import Time
+
+#for date in YYYYMMDD, time in HH:MM:SS, location in Google Maps format
+def return_zenith(date,time='12:00:00',location,frame='icrs'):
+    mod_date = f'{date[:4]}-{date[4:6]}-{date[6:]}'
+    call_time = Time(f'{mod_date}T{time}')
+    loc = EarthLocation(location)
+    
+    coord = SkyCoord(0,90,frame=AltAz(obstime=call_time,location=loc),unit=u.deg)
+    return coord.transform_to(frame)
