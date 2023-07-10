a1638e052241a3adf04c161863b53debcb32f5bf
Theo Richardson <richardson.t@ufl.edu>
2023-07-10 16:19:07 -0500

    fix keyword order

--- make_coord.py
+++ make_coord.py
@@ -3,7 +3,7 @@ from astropy.coordinates import SkyCoord,AltAz,EarthLocation
 from astropy.time import Time
 
 #for date in YYYYMMDD, time in HH:MM:SS, location in Google Maps format
-def return_zenith(date,time='12:00:00',location,frame='icrs'):
+def return_zenith(date,location,time='12:00:00',frame='icrs'):
     mod_date = f'{date[:4]}-{date[4:6]}-{date[6:]}'
     call_time = Time(f'{mod_date}T{time}')
     loc = EarthLocation(location)
