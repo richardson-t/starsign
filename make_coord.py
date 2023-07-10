ae53779ce5bd0fc4cc76418f9db767e921d65a8e
Theo Richardson <richardson.t@ufl.edu>
2023-07-10 16:29:18 -0500

    fix location creation

--- make_coord.py
+++ make_coord.py
@@ -6,7 +6,7 @@ from astropy.time import Time
 def return_zenith(date,location,time='12:00:00',frame='icrs'):
     mod_date = f'{date[:4]}-{date[4:6]}-{date[6:]}'
     call_time = Time(f'{mod_date}T{time}')
-    loc = EarthLocation(location)
+    loc = EarthLocation.of_address(location)
     
     coord = SkyCoord(0,90,frame=AltAz(obstime=call_time,location=loc),unit=u.deg)
     return coord.transform_to(frame)
