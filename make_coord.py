e33e3c9b402a99955365ad1c96d18a39fd192f13
Leo Skaer <lskaer@hawaii.edu>
2023-07-11 14:17:44 -0500

    Added timezone functionality

--- make_coord.py
+++ make_coord.py
@@ -1,12 +1,21 @@
+import pytz
+import datetime
+from timezonefinder import TimezoneFinder
 from astropy import units as u
 from astropy.coordinates import SkyCoord,AltAz,EarthLocation
 from astropy.time import Time
+tf = TimezoneFinder()
 
 #for date in YYYYMMDD, time in HH:MM:SS, location in Google Maps format
 def return_zenith(date,location,time='12:00:00',frame='icrs'):
     mod_date = f'{date[:4]}-{date[4:6]}-{date[6:]}'
     call_time = Time(f'{mod_date}T{time}')
     loc = EarthLocation.of_address(location)
+    geo_loc = loc.to_geodetic()
+    tz = tf.timezone_at(lng=geo_loc.lon.value, lat=geo_loc.lat.value)
+    timezone = pytz.timezone(tz)
+    dt = call_time.to_datetime()
+    new_dt = dt - timezone.utcoffset(dt)
     
-    coord = SkyCoord(0,90,frame=AltAz(obstime=call_time,location=loc),unit=u.deg)
+    coord = SkyCoord(0,90,frame=AltAz(obstime=new_dt.isoformat(),location=loc),unit=u.deg)
     return coord.transform_to(frame)
