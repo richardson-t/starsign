6db7b425af761f3ff0ba122c9eb3aab15aa4ad06
Leo Skaer <lskaer@hawaii.edu>
2023-07-13 14:38:41 -0500

    nevermind

--- starsign/starsign.py
+++ starsign/starsign.py
@@ -4,6 +4,8 @@ from astropy import units as u
 from timezonefinder import TimezoneFinder
 from pytz import timezone
 from astroquery.simbad import Simbad
+custom_simbad = Simbad()
+custom_simbad.add_votable_fields('otype','otype(opt)', 'otypes')
 
 class StarSign(object):    
     def __init__(self,location,date,time='12:00:00',frame='icrs'):
@@ -53,67 +55,9 @@ class StarSign(object):
         ra = self.__coord.ra.value
         dec = self.__coord.dec.value
         sign = '+' if dec >= 0 else ''
-        stars = Simbad.query_criteria(f'region(circle, ICRS, {ra} {sign}{dec}, 0.5d)',maintype='Star', otypes=['Ma*','MS*','Ev*','**','LM*','V*','Em*','PM*','HV*'])
+        stars = custom_simbad.query_criteria(f'region(circle, ICRS, {ra} {sign}{dec}, 0.5d)', otype='Star')
         return stars[0]
 
     @property
     def star(self,radius='0d20m0s'):
         return self.__star
-
-otype_d = {
-    'Ma*' : 'Massive Star',
-    'bC*' : 'Beta Cepheid Variable',
-    'sg*' : 'Evolved Supergiant',
-    's*r' : 'Red Supergiant',
-    's*y' : 'Yellow Supergiant',
-    's*b' : 'Blue Supergiant',
-    'WR*' : 'Wolf-Rayet',
-    'N*' : 'Neutron Star',
-    'Psr' : 'Pulsar',
-    'MS*' : 'Main Sequence Star',
-    'Be*' : 'Be Star',
-    'BS*' : 'Blue Straggler',
-    'gD*' : 'Gamma Doradus Variable Star',
-    'dS*' : 'Delta Scuti Variable Star',
-    'Ev*' : 'Evolved Star',
-    'RG*' : 'Red Giant Branch Star',
-    'HS*' : 'Hot Subdwarf',
-    'HB*' : 'Horizontal Branch Star',
-    'RR*' : 'RR Lyrae Variable Star',
-    'WV*' : 'Type II Cepheid Variable Star',
-    'Ce*' : 'Cepheid Variable Star',
-    'cC*' : 'Classical Cepheid Variable Star',
-    'C*' : 'Carbon Star',
-    'S*' : 'S-type Star',
-    'LP*' : 'Long-Period Variable Star',
-    'AB*' : 'Asymptotic Giant Branch Star',
-    'Mi*' : 'Mira Variable Star',
-    'OH*' : 'OH/IR Star',
-    'pA*' : 'Post-AGB Star',
-    'RV*' : 'RV Tauri Variable Star',
-    'PN' : 'Planetary Nebula',
-    'WD*' : 'White Dwarf',
-    '**' : 'Double or Multiple Star',
-    'El*' : 'Ellipsoidal Variable',
-    'EB*' : 'Eclipsing Binary',
-    'SB*' : 'Spectroscopic Binary',
-    'BY*' : 'BY Draconis Variable',
-    'RS*' : 'RS Canum Venaticorum Variable',
-    'Sy*' : 'Symbiotic Star',
-    'XB*' : 'X-ray Binary',
-    'LXB' : 'Low Mass X-ray Binary',
-    'HXB' : 'High Mass X-ray Binary',
-    'CV*' : 'Cataclysmic Binary',
-    'No*' : 'Classical Nova',
-    'SN*' : 'Supernova',
-    'LM*' : 'Low Mass Star',
-    'BD*' : 'Brown Dwarf',
-    'V*' : 'Variable Star',
-    'Ir*' : 'Irregular Variable Star',
-    'Er*' : 'Eruptive Variable Star',
-    'Ro*' : 'Rotating Variable Star',
-    'Pu*' : 'Pulsating Variable Star',
-    'Em*' : 'Emission-line Star',
-    'PM*' : 'High Proper Motion Star',
-    'HV*' : 'High Velocity Star'
-}
